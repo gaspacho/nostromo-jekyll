53ee3218b53c3e5d196d99f57c0da3aba99be3cc
gaspacho <sergio.pernas@gmail.com>
2014-03-04 15:35:25 -0300

    css

---
+++ css/pers.cs
@@ -0,0 +1,14 @@
+body {
+background-color: #ffffff;
+}
+
+.center { margin: 0 auto; }
+
+.container {
+/* Para CSS3 */
+background-color: rgba(255,255,255,.5);
+/* Para anteriores a IE8 */ 
+filter: progid:DXImageTransform.Microsoft.gradient(GradientType=0, StartColorstr='#88ff0000', EndColorstr='#88ff0000');
+/* Para IE8 */
+-ms-filter: progid:DXImageTransform.Microsoft.gradient(GradientType=0, StartColorstr='#88ff0000', EndColorstr='#88ff0000');
+}
